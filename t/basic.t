5477cf921a00a43ba9997b9eb5358bab19247cee
Daisuke Murase <typester@cpan.org>
2012-05-02 16:32:36 +0900

    initial commit

---
+++ t/basic.t
@@ -0,0 +1,18 @@
+use strict;
+use warnings;
+
+use Test::More;
+
+use_ok 'Data::Apple::PriceTier';
+
+is Data::Apple::PriceTier->price_for( country => 'Japan', tier => 1 ), '85', 'japan tier 1 ok';
+is Data::Apple::PriceTier->price_for( currency => 'Yen', tier => 2 ), '170', 'yen tier 2 ok';
+
+is Data::Apple::PriceTier->proceed_for( country => 'U.S.', tier => 87 ), '700', 'U.S. proceed 87 ok';
+
+
+my $tier_jp = Data::Apple::PriceTier->new( country => 'Japan' );
+is $tier_jp->price_for_tier(1), '85', 'tier 1 jp ok';
+is $tier_jp->proceed_for_tier(2), '119', 'tier 2 jp ok';
+
+done_testing;
---
+++ tools/parse_pricematrix_html.pl
@@ -0,0 +1,98 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Pod::Usage;
+use HTML::TreeBuilder::XPath;
+use HTML::Selector::XPath 'selector_to_xpath';
+
+my $file = $ARGV[0] or pod2usage(-1);
+
+my $tree = HTML::TreeBuilder::XPath->new_from_file($file);
+
+my @tr = $tree->findnodes(selector_to_xpath('table tr'));
+
+my @header_td = $tr[0]->find_by_tag_name('td');
+shift @header_td;
+
+my (@countries, @currencies);
+for my $country_and_currency (map { $_->as_trimmed_text } @header_td) {
+    my ($country, $currency) = split ' - ', $country_and_currency;
+
+    push @countries, $country;
+    push @currencies, $currency;
+}
+
+print 'our %COUNTRIES = (', "\n";
+my $i = 0;
+for my $country (@countries) {
+    print "    '$country' => @{[ $i++ ]},\n";
+}
+print ");\n";
+
+print "\n";
+
+print 'our %CURRENCIES = (', "\n";
+$i = 0;
+for my $currency (@currencies) {
+    print "    '$currency' => @{[ $i++ ]},\n";
+}
+print ");\n"; 
+print "\n";
+
+splice @tr, 0, 2;               # remove header tr's
+
+my $price_matrix   = [];
+my $proceed_matrix = [];
+
+for my $t (@tr) {
+    my @td = $t->find_by_tag_name('td');
+    shift @td;
+
+    my (@prices, @proceeds);
+
+    while (scalar @td) {
+        push @prices, (shift @td)->as_trimmed_text;
+        push @proceeds, (shift @td)->as_trimmed_text;
+    }
+
+    push @$price_matrix, \@prices;
+    push @$proceed_matrix, \@proceeds;
+}
+
+$tree->delete;
+
+print 'our $PRICE_MATRIX = [', "\n";
+for my $row (@$price_matrix) {
+    print '    [';
+    print join ',', map { sprintf '%8s', $_ } @$row;
+    print "],\n";
+}
+print "];\n";
+print "\n";
+
+print 'our $PROCEED_MATRIX = [', "\n";
+for my $row (@$proceed_matrix) {
+    print '    [';
+    print join ',', map { sprintf '%8s', $_ } @$row;
+    print "],\n";
+}
+print "];\n";
+print "\n";
+
+__END__
+
+=head1 NAME
+
+parse_pricematrix_html.pl - parse price matrix html provided by Apple
+
+=head1 SYNOPSIS
+
+    parse_pricematrix_html.pl pricematrix.html
+
+=head1 AUTHOR
+
+Daisuke Murase <typester@cpan.org>
+
+=cut
